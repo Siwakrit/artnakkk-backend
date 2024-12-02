d7f9aaad8bb168c25532c3eb92b36cdcfb8d3109
SIWAKRIT PATTAVEESIN <mr.siwakrit@gmail.com>
2024-12-02 15:22:33 +0700

    Pro

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.json());
 //Define the list of allowed origins
 const allowedOrigins = [
   "https://artnakkk-admin.vercel.app",
+  "https://artnakkk-admin-siwakrit-pattaveesins-projects.vercel.app",
   "http://localhost:5173", // For local development
   "http://localhost:5174", // For local development
   "http://localhost:5175", // For local development
